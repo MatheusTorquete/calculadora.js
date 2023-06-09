3493233707a66b89f6d9931d8afaa4652f4d4ba9
Matheus <matheuswe10@gmail.com>
2023-06-08 22:22:49 -0300

    segunda parte calculadora javascript

--- calculadora.js
+++ calculadora.js
@@ -19,29 +19,30 @@ const operacaoPendente = () => operador !== undefined;
 // Função para realizar o cálculo
 const calcular = () => {
     if (operacaoPendente()) {
-        const numeroAtual = parseFloat(display.textContent);
+        const numeroAtual = parseFloat(display.textContent.replace(',', '.'));
         novoNumero = true;
-        
+        const resultado = eval (`${numeroAnterior}${operador}${numeroAtual}`);
+        atualizarDisplay(resultado);
         // Executa a operação com base no operador selecionado
-        if (operador == '+') {
-            atualizarDisplay(numeroAnterior + numeroAtual);
-        } else if (operador == '-') {
-            atualizarDisplay(numeroAnterior - numeroAtual);
-        } else if (operador == '*') {
-            atualizarDisplay(numeroAnterior * numeroAtual);
-        } else if (operador == '/') {
-            atualizarDisplay(numeroAnterior / numeroAtual);
-        }
+        // if (operador == '+') {
+        //     atualizarDisplay(numeroAnterior + numeroAtual);
+        // } else if (operador == '-') {
+        //     atualizarDisplay(numeroAnterior - numeroAtual);
+        // } else if (operador == '*') {
+        //     atualizarDisplay(numeroAnterior * numeroAtual);
+        // } else if (operador == '/') {
+        //     atualizarDisplay(numeroAnterior / numeroAtual);
+        // }
     }
 }
 
 // Função para atualizar o conteúdo do display
 const atualizarDisplay = (texto) => {
     if (novoNumero) {
-        display.textContent = texto;
+        display.textContent = texto.toLocaleString('BR');
         novoNumero = false;
     } else {
-        display.textContent += texto;
+        display.textContent += texto.toLocaleString('BR');
     }
 }
 
@@ -54,7 +55,52 @@ const selecionarOperador = (evento) => {
     if (!novoNumero) {
         novoNumero = true;
         operador = evento.target.textContent;
-        numeroAnterior = parseFloat(display.textContent);
+        numeroAnterior = parseFloat(display.textContent.replace(',', '.'));
     }
 }
 operadores.forEach(operador => operador.addEventListener('click', selecionarOperador));
+
+// Função para ativar o botão de igual
+const ativarIgual = () => {
+    calcular();
+    operador = undefined;
+}
+document.getElementById('igual').addEventListener('click', ativarIgual);
+
+// Função para limpar o display
+const limparDisplay = () => display.textContent = '';
+document.getElementById('limparDisplay').addEventListener('click', limparDisplay);
+
+// Função para limpar o cálculo
+const limparCalculo = () => {
+    limparDisplay();
+    operador = undefined;
+    novoNumero = true;
+    numeroAnterior = undefined;
+}
+document.getElementById('limparCalculo').addEventListener('click', limparCalculo);
+
+// Função para remover o último número digitado
+const removerUltimoNumero = () => display.textContent = display.textContent.slice(0, -1);
+document.getElementById('backspace').addEventListener('click', removerUltimoNumero);
+
+// Função para inverter o sinal do número
+const inverterSinal = () => {
+    novoNumero = true;
+    atualizarDisplay(display.textContent * -1);
+};
+document.getElementById('inverter').addEventListener('click', inverterSinal);
+
+// Funções auxiliares para o botão decimal
+const existeDecimal = () => display.textContent.indexOf(',') !== 1;
+const existeValor = () => display.textContent.length > 0;
+const inserirDecimal = () => {
+    if (!existeDecimal()) {
+        if (existeValor) {
+            atualizarDisplay(',');
+        } else {
+            atualizarDisplay('0,');
+        }
+    }
+}
+document.getElementById('decimal').addEventListener('click', inserirDecimal);
